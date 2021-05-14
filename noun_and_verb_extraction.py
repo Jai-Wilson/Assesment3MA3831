4140eab071b36f7718670b2f46e01b7941dc0042
Jai Wilson <jai.wilson@my.jcu.edu.au>
2021-05-14 16:53:27 +1000

    Add stop word removal and lemmatise the data

--- noun_and_verb_extraction.py
+++ noun_and_verb_extraction.py
@@ -8,10 +8,12 @@ from nltk.corpus import stopwords
 import tqdm
 from collections import Counter
 import operator
+from nltk.stem import WordNetLemmatizer
 import matplotlib.pyplot as plt
 
 nlp = spacy.load("en_core_web_sm")
 stop_words = set(stopwords.words('english'))
+lemmatizer = WordNetLemmatizer()
 
 
 def remove_stop_words(dataset):
@@ -30,12 +32,25 @@ def remove_stop_words(dataset):
 
     return dataset
 
+def lemmatise_dataset(dataset):
+    for i in range(len(dataset)):
+        current_description = dataset.iloc[i]["Post Description"]
+        word_list = nltk.word_tokenize(current_description)
+        lemmatized_output = ' '.join([lemmatizer.lemmatize(w) for w in word_list])
+        dataset.iloc[i]["Post Description"] = lemmatized_output
+
+        return dataset
+
+
+
+
+
 
 # add common car terms to stop words list as they do nor provide information on part failures
-
-
-
-
+stop_words.add('car')
+stop_words.add('problem')
+stop_words.add('issue')
+stop_words.add("Thanks")
 
 top_dataframe = pd.read_csv("Top brand.csv")
 second_dataframe = pd.read_csv("Second brand.csv")
@@ -43,23 +58,34 @@ third_dataframe = pd.read_csv("Third brand.csv")
 fourth_dataframe = pd.read_csv("Fourth brand.csv")
 fifth_dataframe = pd.read_csv("Fifth brand.csv")
 
-print(top_dataframe)
-print("removed stopwords")
-top_dataframe = remove_stop_words(top_dataframe)
-print(top_dataframe)
-print(second_dataframe)
-print(third_dataframe)
-print(fourth_dataframe)
-print(fifth_dataframe)
 
-descriptions = top_dataframe["Post Description"]
+top_dataframe = remove_stop_words(top_dataframe)
+top_dataframe = lemmatise_dataset(top_dataframe)
+
+second_dataframe = remove_stop_words(second_dataframe)
+second_dataframe = lemmatise_dataset(second_dataframe)
+
+third_dataframe = remove_stop_words(third_dataframe)
+third_dataframe = lemmatise_dataset(third_dataframe)
+
+fourth_dataframe = remove_stop_words(fourth_dataframe)
+fourth_dataframe = lemmatise_dataset(fourth_dataframe)
+
+fifth_dataframe = remove_stop_words(fifth_dataframe)
+fifth_dataframe = lemmatise_dataset(fifth_dataframe)
+
+
+
+
+
+descriptions = fourth_dataframe["Post Description"]
 descriptions = descriptions.tolist()
 top_nouns = []
 
 for description in tqdm.tqdm(descriptions):
     words = nlp(description)
     for word in words:
-        #print(word.text, word.pos_)
+        # print(word.text, word.pos_)
         if word.pos_ == "NOUN":
             top_nouns.append(word.text)
 print(top_nouns)
@@ -67,6 +93,3 @@ top_nouns_found = Counter(top_nouns)
 top_nouns_ordered = sorted(top_nouns_found.items(), key=operator.itemgetter(1), reverse=True)
 top__nouns_ordered = top_nouns_ordered[0:5]
 print(top_nouns_ordered)
-
-
-
