29576183815b2b8902177c62dd186350dd28081c
Jai Wilson <jai.wilson@my.jcu.edu.au>
2021-05-16 20:54:56 +1000

    Store the top 5 nouns for each car brand in one list

--- noun_and_verb_extraction.py
+++ noun_and_verb_extraction.py
@@ -32,6 +32,7 @@ def remove_stop_words(dataset):
 
     return dataset
 
+
 def lemmatise_dataset(dataset):
     for i in range(len(dataset)):
         current_description = dataset.iloc[i]["Post Description"]
@@ -40,10 +41,6 @@ def lemmatise_dataset(dataset):
         dataset.iloc[i]["Post Description"] = lemmatized_output
 
         return dataset
-
-
-
-
 
 
 # add common car terms to stop words list as they do nor provide information on part failures
@@ -57,7 +54,6 @@ second_dataframe = pd.read_csv("Second brand.csv")
 third_dataframe = pd.read_csv("Third brand.csv")
 fourth_dataframe = pd.read_csv("Fourth brand.csv")
 fifth_dataframe = pd.read_csv("Fifth brand.csv")
-
 
 top_dataframe = remove_stop_words(top_dataframe)
 top_dataframe = lemmatise_dataset(top_dataframe)
@@ -74,22 +70,41 @@ fourth_dataframe = lemmatise_dataset(fourth_dataframe)
 fifth_dataframe = remove_stop_words(fifth_dataframe)
 fifth_dataframe = lemmatise_dataset(fifth_dataframe)
 
-
-
-
-
-descriptions = top_dataframe["Post Description"]
-descriptions = descriptions.tolist()
 top_nouns = []
 
-for description in tqdm.tqdm(descriptions):
-    words = nlp(description)
-    for word in words:
-        # print(word.text, word.pos_)
-        if word.pos_ == "NOUN":
-            top_nouns.append(word.text)
-print(top_nouns)
-top_nouns_found = Counter(top_nouns)
-top_nouns_ordered = sorted(top_nouns_found.items(), key=operator.itemgetter(1), reverse=True)
-top_nouns_ordered = top_nouns_ordered[0:5]
-print(top_nouns_ordered)
+for i in range(0, 5):
+    if i == 0:
+        descriptions = top_dataframe["Post Description"]
+        descriptions = descriptions.tolist()
+    elif i == 1:
+        descriptions = second_dataframe["Post Description"]
+        descriptions = descriptions.tolist()
+    elif i == 2:
+        descriptions = third_dataframe["Post Description"]
+        descriptions = descriptions.tolist()
+    elif i == 3:
+        descriptions = fourth_dataframe["Post Description"]
+        descriptions = descriptions.tolist()
+    elif i == 4:
+        descriptions = fifth_dataframe["Post Description"]
+        descriptions = descriptions.tolist()
+    current_nouns = []
+    for description in tqdm.tqdm(descriptions):
+        words = nlp(description)
+        for word in words:
+            # print(word.text, word.pos_)
+            if word.pos_ == "NOUN":
+                current_nouns.append(word.text)
+    top_nouns_found = Counter(current_nouns)
+    top_nouns_ordered = sorted(top_nouns_found.items(), key=operator.itemgetter(1), reverse=True)
+    top_nouns_ordered = top_nouns_ordered[0:5]
+    # print(top_nouns_ordered)
+    top_nouns.append(top_nouns_ordered)
+
+# print the list vertically to be seen easier from the user
+for a, b, c, d, e in zip(*top_nouns):
+    print(a, b, c, d, e)
+
+
+
+
--- storagedoc.py
+++ storagedoc.py
@@ -1,89 +1,36 @@
-import bs4
-from bs4 import BeautifulSoup as soup
-from urllib.request import urlopen as uReq
-import tqdm
+'''A simple document for storing code that may/may not be used for the NLP tasks'''
 
-general_url = "https://www.ericthecarguy.com/forums/forum/stay-dirty-lounge/service-and-repair-questions-answered-here/"
+# descriptions = top_dataframe["Post Description"]
+# descriptions = descriptions.tolist()
+top_nouns = []
 
-filename = "example.csv"
-f = open(filename, "a")
+for i in range(0, 5):
+    if i == 0:
+        descriptions = top_dataframe["Post Description"]
+        descriptions = descriptions.tolist()
+    elif i == 1:
+        descriptions = second_dataframe["Post Description"]
+        descriptions = descriptions.tolist()
+    elif i == 2:
+        descriptions = third_dataframe["Post Description"]
+        descriptions = descriptions.tolist()
+    elif i == 3:
+        descriptions = fourth_dataframe["Post Description"]
+        descriptions = descriptions.tolist()
+    elif i == 4:
+        descriptions = fifth_dataframe["Post Description"]
+        descriptions = descriptions.tolist()
+    current_nouns = []
+    for description in tqdm.tqdm(descriptions):
+        words = nlp(description)
+        for word in words:
+            # print(word.text, word.pos_)
+            if word.pos_ == "NOUN":
+                current_nouns.append(word.text)
+    top_nouns_found = Counter(current_nouns)
+    top_nouns_ordered = sorted(top_nouns_found.items(), key=operator.itemgetter(1), reverse=True)
+    top_nouns_ordered = top_nouns_ordered[0:5]
+    print(top_nouns_ordered)
+    top_nouns.append(top_nouns_ordered)
 
-headers = "Date Posted,Author,Title of Post,Post Description\n"
-
-f.write(headers)
-
-# got to page 501
-
-for j in tqdm.tqdm(range(3, 10)):
-    print("Up to page {}".format(j))
-    if j != 1:
-        current_url = general_url + "page/" + "{}".format(j) + "/"
-    else:
-        current_url = general_url
-
-    for i in range(0, 15):
-
-        try:
-            # opening connection, grabbing the page
-            uClient = uReq(current_url)
-            page_html = uClient.read()
-            uClient.close()
-
-            # html parsing
-            page_soup = soup(page_html, "html.parser")
-
-            # grabs each topic
-            # find all where class is bhp-body
-            containers = page_soup.findAll("li", {"class": "bbp-topic-title"})
-            # inspect each container
-            # inspect the a tag
-            container = containers[i + 1]  # this is indexed depending on the article
-            new_url = container.a['href']
-            # get the url that references what the user has said
-
-            person_posted = page_soup.findAll("span", {"class": "bbp-topic-started-by"})
-            author_name = person_posted[i].a.img["alt"]  # indexed depending on the article
-
-            # opening connection, grabbing the page
-            uClient = uReq(new_url)
-            new_page_html = uClient.read()
-            uClient.close()
-            new_page_soup = soup(new_page_html, "html.parser")
-            # extract the title
-            forum_title = new_page_soup.h1.span.text
-
-            # extract the article description
-            new_containers = new_page_soup.findAll("div", {"class": "bbp-topic-content"})
-            # text is contained in p tags
-            text = new_containers[1].find_all('p')
-
-            description = ""
-            for i in range(len(text)):
-                # extracts the raw text from the tag
-                current_decription = text[i].text
-                description = description + current_decription
-
-            date_container = new_page_soup.findAll("span", {"class": "bbp-topic-post-date"})
-            date_posted = date_container[0].text
-        except:
-            pass
-
-        # some dickhead decided to post url's, this is so annoying, do a try catch and if it catches just pass it
-        try:
-
-            date_posted = date_posted.replace(", ", " ")
-
-            forum_title = forum_title.replace(",", " ")
-            forum_title = forum_title.replace("  ", " ")
-
-            description = description.replace("\r\n", " ")
-            description = description.replace(",", " ")
-            description = description.replace("  ", " ")
-            description = description.replace("\n", "")
-
-            f.write(date_posted.replace(",", " ") + "," + author_name + "," + forum_title.replace(",", " ") + "," +
-                    description + "\n")
-        except:
-            pass
-
-f.close()
+print(top_nouns)
