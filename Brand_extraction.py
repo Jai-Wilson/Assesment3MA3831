558861baa44052a2bdffb666a3f610d1d2b49415
Jai Wilson <jai.wilson@my.jcu.edu.au>
2021-05-18 18:58:59 +1000

    misc commit for version control

--- Brand_extraction.py
+++ Brand_extraction.py
@@ -19,7 +19,7 @@ scraped_data = pd.read_csv("scraped_info.csv")
 
 scraped_data = scraped_data[["Title of Post", "Post Description"]]
 
-# remove any not-utf* encoded characters
+# remove any not-utf8 encoded characters
 scraped_data["Title of Post"] = scraped_data["Title of Post"].str.encode("ascii", "ignore").str.decode("ascii")
 scraped_data["Post Description"] = scraped_data["Post Description"].str.encode("ascii", "ignore").str.decode("ascii")
 
