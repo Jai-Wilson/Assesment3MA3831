28ead08e7e832d293718fba27e6e62bd8f7c2fd5
Jai Wilson <jai.wilson@my.jcu.edu.au>
2021-05-30 16:46:09 +1000

    Add code in test_file.py for evidence of webscraper, update notebook documents

--- test_file.py
+++ test_file.py
@@ -1,80 +1,102 @@
-"""Test file to test code before it is implemented into assignment. Mianly to make sure that nothing breaks before
-I mess with my data"""
+"""File that scrapes the Eric the Car guy. The specific page that is scraped is a service and repairs forum.
+This forum and page was chosen as this page will mention specifc car brands, models, hat part is failing and
+verbs associated with the parts that are failing."""
 
-
-import spacy
-import pandas as pd
-import nltk
-from nltk import word_tokenize, pos_tag, ne_chunk
+import bs4
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen as uReq
 import tqdm
-from collections import Counter
-import operator
-import matplotlib.pyplot as plt
-from wordcloud import WordCloud
 
-nlp = spacy.load("en_core_web_sm")
-# might need to use large?
+general_url = "https://www.ericthecarguy.com/forums/forum/stay-dirty-lounge/service-and-repair-questions-answered-here/"
 
-scraped_data = pd.read_csv("example.csv")
+filename = "example.csv"
+f = open(filename, "w")
 
-scraped_data = scraped_data[["Title of Post", "Post Description"]]
+headers = "Date Posted,Author,Title of Post,Post Description\n"
 
-# remove any not-utf8 encoded characters
-scraped_data["Title of Post"] = scraped_data["Title of Post"].str.encode("ascii", "ignore").str.decode("ascii")
-scraped_data["Post Description"] = scraped_data["Post Description"].str.encode("ascii", "ignore").str.decode("ascii")
-
-titles = scraped_data["Title of Post"]
-titles = titles.tolist()
-
-descriptions = scraped_data["Post Description"]
-descriptions = descriptions.tolist()
-
-organisations_titles = []
-
-for title in tqdm.tqdm(titles):
-    try:
-        title_raw = nlp(title)
-        for word in title_raw.ents:
-            if word.label_ == "ORG":
-                organisations_titles.append(word.text)
-    except:
-        pass
-
-print("titles")
-top_organisations_titles = Counter(organisations_titles)
-top_organisations_titles_ordered = sorted(top_organisations_titles.items(), key=operator.itemgetter(1), reverse=True)
-top_five_organisations = top_organisations_titles_ordered[0:5]
-print(top_five_organisations)
-
-top_organisations_dict = dict(top_five_organisations)
-
-# extract the most common brands as a list
-organisations_list = list(top_organisations_dict.keys())
-
-top_ten_keys = top_organisations_dict.keys()
-values = top_organisations_dict.values()
-plt.bar(top_ten_keys, values)
-plt.xticks(rotation=75)
-plt.xlabel("Most commonly recognised entities")
-plt.ylabel("Frequency")
-plt.title("Most common entities recognised in scraped database")
-plt.show()
+f.write(headers)
 
 
+# range of for loop is page start and page end
+for j in tqdm.tqdm(range(1, 2)):
+    print("Up to page {}".format(j))
+    if j != 1:
+        current_url = general_url + "page/" + "{}".format(j) + "/"
+    else:
+        current_url = general_url
 
+    for i in range(0, 15):
 
+        try:
+            # opening connection, grabbing the page
+            uClient = uReq(current_url)
+            page_html = uClient.read()
+            uClient.close()
 
+            # html parsing
+            page_soup = soup(page_html, "html.parser")
 
+            # grabs each topic
+            # find all where class is bhp-topic-title
+            containers = page_soup.findAll("li", {"class": "bbp-topic-title"})
+            # inspect each container
+            # inspect the a tag
+            container = containers[i + 1]  # this is indexed depending on the article
+            print(container)
+            new_url = container.a['href']
+            # get the url that references what the user has said
+            print("new URL is {}".format(new_url))
 
+            person_posted = page_soup.findAll("span", {"class": "bbp-topic-started-by"})
+            print(person_posted)
+            author_name = person_posted[i].a.img["alt"]  # indexed depending on the article
+            print("The author of the post is {}".format(author_name))
 
+            # opening connection, grabbing the page
+            uClient = uReq(new_url)
+            new_page_html = uClient.read()
+            uClient.close()
+            new_page_soup = soup(new_page_html, "html.parser")
+            # extract the title
+            forum_title = new_page_soup.h1.span.text
+            print("Forum title is: {}".format(forum_title))
 
+            # extract the article description
+            new_containers = new_page_soup.findAll("div", {"class": "bbp-topic-content"})
+            # text is contained in p tags
+            text = new_containers[1].find_all('p')
+            print("Raw description is: {}".format(text))
 
+            description = ""
+            for i in range(len(text)):
+                # extracts the raw text from the tag
+                current_decription = text[i].text
+                description = description + current_decription
 
+            date_container = new_page_soup.findAll("span", {"class": "bbp-topic-post-date"})
+            print(date_container)
+            date_posted = date_container[0].text
+            print("Date posted is: {}".format(date_posted))
+        except:
+            pass
 
+        # some users might post a url to another site, this causes the program to crash. If this occurs, simply pass
+        # over the post and disregard it.
+        try:
 
+            date_posted = date_posted.replace(", ", " ")
 
+            forum_title = forum_title.replace(",", " ")
+            forum_title = forum_title.replace("  ", " ")
 
+            description = description.replace("\r\n", " ")
+            description = description.replace(",", " ")
+            description = description.replace("  ", " ")
+            description = description.replace("\n", "")
 
+            f.write(date_posted.replace(",", " ") + "," + author_name + "," + forum_title.replace(",", " ") + "," +
+                    description + "\n")
+        except:
+            pass
 
-
-
+f.close()
